046e51b550508bc0ffb24402bcb5091d7ebef73f
Glowkik <glowkikofficial@gmail.com>
2026-06-13 16:57:25 +0530

    fix auth onclose

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+﻿import { useState, useEffect } from "react";
 import { auth } from "./firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile, sendEmailVerification } from "firebase/auth";
 
